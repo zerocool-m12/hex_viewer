f16113ebaf8ca7d018b18fcedfb8d283f8bf4f54
zerocool_m12 <zerocool_m12@hotmail.com>
2024-02-21 21:27:18 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -52,29 +52,29 @@ void process(const std::string&path){
   for(int i = 0;i<buff.size();i++){
     if(counter == 0){
       std::cout<<std::uppercase<<std::setw(8)<<std::setfill('0')<<std::hexfloat<<std::hex<<16*address++<<": ";   // this is how to write a hexidecimal number in 16 bit format
-      std::cout<<std::uppercase<<std::setw(2)<<std::hex<<static_cast<int>(buff[i]);
-      if(buff[i] == '\n' or buff[i] == '\t') temp.push_back('.');
+      std::cout<<std::uppercase<<std::setw(2)<<std::setfill('0')<<std::hex<<(static_cast<short int>(buff[i]) bitand 0xff);
+      if(not isprint(buff[i])) temp.push_back('.');
       else temp.push_back(buff[i]);
       space = not(space);
       counter++;
       }
    else if(counter>0 and counter <15 and space == true){
-      std::cout<<std::uppercase<<std::setw(2)<<std::hex<<static_cast<int>(buff[i])<<" ";
+      std::cout<<std::uppercase<<std::setw(2)<<std::setfill('0')<<std::hex<<(static_cast<short int>(buff[i]) bitand 0xff)<<" ";  // masking
       space = false;
-      if(buff[i] == '\n' or buff[i] =='\t') temp.push_back('.');
+      if(not isprint(buff[i])) temp.push_back('.');
       else temp.push_back(buff[i]);
       counter++;
       }
     else if (counter >0 and counter <15 and space == false){
-      std::cout<<std::uppercase<<std::setw(2)<<std::hex<<static_cast<int>(buff[i]);
+      std::cout<<std::uppercase<<std::setw(2)<<std::setfill('0')<<std::hex<<(static_cast<short int>(buff[i]) bitand 0xff);
       space = true;
-      if(buff[i] == '\n' or buff[i] =='\t') temp.push_back('.');
+      if(not isprint(buff[i])) temp.push_back('.');
       else temp.push_back(buff[i]);
       counter++;
       }
    else if(counter == 15){
-      std::cout<<std::uppercase<<std::setw(2)<<std::hex<<static_cast<int>(buff[i])<<"  ";
-      if(buff[i] == '\n' or buff[i] == '\t') temp.push_back('.');
+      std::cout<<std::uppercase<<std::setw(2)<<std::setfill('0')<<std::hex<<(static_cast<short int>(buff[i]) bitand 0xff)<<"  ";
+      if(not isprint(buff[i])) temp.push_back('.');
       else temp.push_back(buff[i]);
       std::cout<<temp<<std::endl;
       counter = 0;
